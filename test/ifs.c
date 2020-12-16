64ab5aa0dcff6ed5ec6719ffa39d5d2dcea9c78b
Mateusz Radomski <radomski.main@protonmail.com>
2020-12-16 20:19:22 +0100

    New ifs test.

---
+++ test/ifs.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+int main(void)
+{
+    for(int i = 0; i < 10; i++)
+    {
+        int a = rand() & 1;
+        
+        if(a)
+        {
+            int b = 0;
+        }
+        
+        if(a)
+        {
+            if(a + 1)
+            {
+                int c = 0;
+            }
+        }
+        
+        if(a) { int a = 0; }
+    }
+}
