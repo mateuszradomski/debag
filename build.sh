ca35f366903d06ab5f24f95b7546f604ae53beba
Mateusz Radomski <radomski.main@protonmail.com>
2021-03-19 22:00:03 +0100

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ optim='-g -fsanitize=address -DDEBUG'
 # Relase build
 # optim='-O2'
 
-opts=$optim'-Wall -Wextra -Wno-write-strings -Wno-unused-function -Wno-class-memaccess -Wno-format-security'
+opts=$optim' -Wall -Wextra -Wno-write-strings -Wno-unused-function -Wno-class-memaccess -Wno-format-security'
 libs='-lGL -ldl -pthread -lX11 -lelf -lz -lcapstone -lglfw -ldwarf -lunwind -lunwind-generic -lunwind-ptrace'
 static_libs='libs/libimgui_static.a'
 g++ $opts -I./ src/debag.cpp $static_libs -o debag $libs
