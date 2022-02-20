085568aafbf2d8697d900aeb78b7c3b10daf25f7
m <m@pop-os.localdomain>
2022-02-20 12:23:41 +0100

    Move tests into src/ and add build.sh

---
+++ test/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Debuging build
+optim='-g -DDEBUG'
+# Relase build
+# optim='-O2'
+
+# Build the testing bins
+mkdir -p bin
+
+for src_file in src/*.c
+do
+    bin_name=$(basename -s .c $src_file)
+    gcc $src_file -o bin/$bin_name
+done
+
+opts=$optim' -Wall -Wextra -Wno-write-strings -Wno-unused-function -Wno-class-memaccess -Wno-format-security'
+libs='-lGL -ldl -pthread -lX11 -lelf -lz -lcapstone -lglfw -ldwarf -lunwind -lunwind-generic -lunwind-ptrace'
+static_libs='libs/libimgui_static.a'
+
